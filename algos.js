6241bfc5c775be7770793cb5ad6cc29658f105c5
Purohit, Ash <ashp2328@gmail.com>
2019-01-22 19:05:21 -0800

    fourth commit

--- algos.js
+++ algos.js
@@ -95,17 +95,55 @@
 
 // Print integers  using While from 2000 to 5280
 
-function print(low, high){
-  while (low <=  high){
-    console.log(low);
-    low++;
-  }
-}
+// function print(low, high){
+//   while (low <=  high){
+//     console.log(low);
+//     low++;
+//   }
+// }
 
-print(10,20);
+// print(10,20);
+
+
+
+
+
+// //  Countdown by Fours
+// // Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+
+// function count(num){
+//   while(num > 0){
+//     console.log(num);
+//     num -=4;
+//   }
+// }
+// count(2016);
+
+
 
 
 //  Flexible countdowns, 
+// // Based on earlier “Countdown by Fours”, 
+// given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, 
+// using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
+
+// function flexible(low, high, mult){
+//   for(var i = high; i >= low; i--){
+//     if (i % mult === 0 ){
+//       console.log(i)
+//     }
+//   }
+// }
+// flexible(3, 17, 5);
+
+
+
+// //  The Final Countdown
+// // This is based on “Flexible Countdown”. 
+// The parameter names are not as helpful, but the problem is essentially identical; don’t be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
+
+
+
 
 // function flexble(high, low, num){
 //   for (x = high; x>= low; x = x-num){
