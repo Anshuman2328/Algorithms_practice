5c6436f8f4733a2abd9bb6840a1545aaab40f76a
Purohit, Ash <ashp2328@gmail.com>
2019-01-24 20:37:33 -0800

    added steps, anagram, capitalization

--- techcoding.js
+++ techcoding.js
@@ -208,3 +208,281 @@
 // %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  END OF Max char $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
 // %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  END OF Max char $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
 // %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  END OF Max char $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+
+// function fizzbuzz(n){
+//     for(var i=1; i<=n;i++){
+//         if(i % 15 === 0){
+//             console.log("Fizzbuzz");
+//         } else if (i % 5 === 0){
+//             console.log("Buzz");
+//         } else if (i % 3 === 0){
+//             console.log("fizz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzbuzz(15);
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Fizz Buzz $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+//  Given an array and size, make subarrays with the length same as size provided
+
+// Solution 1: 
+// This one uses a for loop, a new array and logic is
+//  take the last element of the new array, if it doesnot exist or if its length is same size then add 
+//  this new element to chunked directly within an array
+// else push this element to the last element(within the chunk)
+// function chunk(array, size){
+//     var chunked = [];
+//     for(let elements of array){
+//         var last = chunked[chunked.length-1];
+//         if(!last || last.length === size){
+//             chunked.push([elements]);
+//         } else {
+//             last.push(elements);
+//         }
+//     }
+//     return chunked;
+// }
+
+// console.log(chunk([1,2,3,4,5,6,5,4,3],4));
+
+
+// Solution 2: create a new array, create a new variable called index, while index < arr.lenght,
+// push the slice of original array into the new array, and increase the index by size
+//  slice method uses two arguments, start and end
+// function newchunk(arr, sze){
+//     var dachunk = [];
+//     var index = 0;
+//     while(index < arr.length){
+//         dachunk.push(arr.slice(index, index+sze));
+//         index = index + sze;
+//     }
+//     return dachunk;
+// }
+
+// console.log(newchunk([1,2,2,3,4,5,6,7,6,4,3,2,3,4],7));
+
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of the Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of the Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of the Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of the Chunk Array $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$% Anagrams $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$% Anagrams $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$% Anagrams $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$% Anagrams $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+
+// Solution 1: this one uses two char maps. Char maps is nothing but a dictionary/object with key value pair
+// to build a dict/object first initiate an empty dict/obj, then use for loop to iterate over the string, 
+// if there is no key then add the value as 1 otherwise increase the current count
+//  then use for in loop to check the lenghts of both dict/obj
+// this is done by using Object.keys(charmap).lenght
+//  if lenght is not same return false other wise check if the keys same or not
+// to do this use for loop to iterate over either one of the charmap using for of loop, if charmap[key] !== second one
+// return false, otherwise return true at the end
+//  edge cases, string with capital letters and other character such as @, ! etc, use regex and apply using replace
+//  for ex, for (let char of str.replace(/[^\w]/g, '').toLowerCase()) this will turn the given string into
+// lowecase with any special characters for easy comparison
+
+// function anagram(str1, str2){
+//     var map1 = {};
+//     var map2 = {};
+//     for(let char of str1){
+//         if(!map1[char]){
+//             map1[char] = 1;
+//         } else {
+//             map1[char] ++;
+//         }
+//     }
+//     for(let char of str2){
+//         if(!map2[char]){
+//             map2[char] = 1;
+//         } else {
+//             map2[char] ++;
+//         }
+//     }
+//     if (Object.keys(map1).length !== Object.keys(map2).length){
+//         return false;
+//     }
+//     for(let element in map1){
+//         if(map1[element] !== map2[element]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// anagram("hello", "helols");
+
+
+// //  Solution 2: this is when you have a helper function
+// function isAnagram(stringA, stringB){
+//     const aCharMap = helperCharMap(stringA);
+//     const bCharMap = helperCharMap(stringB);
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+//         return false;
+//     } 
+//     for (let char in aCharMap){
+//         if(aCharMap[char !== bCharMap[char]]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// function helperCharMap(str){
+//     const charMap= {};
+//     for(let char of str.replace(/[^\w]/g, '').toLowerCase()){
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+// }
+// console.log(anagram("thisisgood", "goodisthis"));
+// console.log(anagram("notgood", "goodnon"));
+// console.log(isAnagram("thisisgood", "goodisthis"));
+// console.log(isAnagram("notgood", "goodnon"));
+// console.log(isAnagram("!!!!thisisgood", "Good!!IS!!THIS"))
+
+
+
+
+// solution 3: this is the quickest solution, the approach is as followes
+//  first clean the given strings, use .replace on a string and in .replace method pass in 
+// regex, lowercase it, split it, sort it and again join it back
+
+// in the main function use === to check if string a is same as string b, by calling helper function on the prvided
+// strings and return it. it wll give truthy and falsy (falsey!!) value
+
+// function solanagram(stringA,stringB){
+//     return cleanstring(stringA) === cleanstring(stringB);
+// }
+// function cleanstring(str){
+//     return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+// }
+// console.log(solanagram("HELLOW", "ewllos"));
+// console.log(solanagram("HELLOW", "ewlloh"));
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Anagram $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Anagram $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Anagram $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Anagram $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+// Solution 1: This is first solution, the idea behind this is to create an empty array
+// split the string with ' '(space) this creates the first word at first index and so on
+// once we have first word at first index, we can then use toUppercase at string[0] and use
+// slice method to join the rest of the characters of the word using slice at argument of 1
+// we can then push this into an array and then join the array back to with the join(' ') (using space again)
+// function cap(str){
+//     var arr = [];
+//     for(let char of str.split(' ')){
+//         arr.push(char[0].toUpperCase() + char.slice(1));
+//     }
+//     return arr.join(' ')
+// }
+// console.log(cap("this is for cap"));
+
+// Solution 2: the idea is to create an empty string, for each element in the string, 
+// if the element has a space to its left, we uppercase it and add it to new empty string
+// otherwise we just add the element to the new string. Remember to always capitalize the very first element since
+//  it is the very first element with no space to its left
+
+// function cap(str){
+//     var result = str[0].toUpperCase();
+//     for(let i = 1; i<str.length; i++){
+//         if(str[i-1] === ' '){
+//             result = result + str[i].toUpperCase();
+//         } else {
+//             result = result + str[i];
+//         }
+//     }
+//     return result;
+// }
+// console.log(cap("this is a test number two"));
+
+//  this is for the test function to check how split with ' ' space works
+// function test(string){
+//     console.log(string[0])
+//     var strr = string.split(' ');
+//     console.log(strr);
+//     console.log(strr[0])
+// }
+// test("this is test")
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of Capitalization $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Steps - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Steps - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Steps - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  Steps - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+
+// soluton 1: this is first solution
+// for each x(row), create a new empty string, then for each y(column) check is y <= x. If it is then 
+// add the step -->> '#' to the matrix(empty string) otherwise just add space ' ' and console log with in first for loop
+
+// function steps(n){
+//     for(var x = 0; x<n; x++){
+//         let matrix = '';
+//         for (var y = 0; y < n; y++){
+//             if( y <= x){
+//                 matrix += '#';
+//             } else {
+//                 matrix += ' ';
+//             }
+//         }
+//         console.log(matrix);
+//     }
+// }
+// steps(3);
+
+
+
+
+// solution 2: 
+
+function steps(n, row = 0, result = ''){
+    if (n === row){
+        return;
+    }
+    if(n === result.length){
+        console.log(result);
+        return steps(n, row + 1);
+    }
+    if(result.length <= row){
+        result += '#';
+    } else {
+        result += ' ';
+    }
+    steps(n, row, result);
+}
+
+steps(14);
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
+// %%$%%%$%$%$%$%$%$%$%$%$%$%$%$%$%  End of - Making a step $%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$
